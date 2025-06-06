b630d0c4f5849f6770bf6393af8c4da9d78f1c55
Olczak_Mikolaj <01159104@pw.edu.pl>
2025-06-06 14:35:49 +0200

    add-cv: added styles, added uploading files and preview files

--- src/app/add-cv/add-cv.component.ts
+++ src/app/add-cv/add-cv.component.ts
@@ -3,18 +3,72 @@ import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 import {HeaderComponent} from "../header/header.component";
 import {NgForOf} from "@angular/common";
 import {RouterLink} from "@angular/router";
-import { faIdCard, faFileArrowUp, faDisplay, faSquareCheck, faPlusCircle, faGears, faStar, faStarHalfStroke } from '@fortawesome/free-solid-svg-icons';
-import { faLinkedinIn, faFacebookF, faXTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faPlusCircle, faGears, faStar, faStarHalfStroke } from '@fortawesome/free-solid-svg-icons';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-add-cv',
-  imports: [FaIconComponent, HeaderComponent, RouterLink],
+  imports: [FaIconComponent, HeaderComponent, RouterLink, NgForOf],
   templateUrl: './add-cv.component.html',
   styleUrl: './add-cv.component.css'
 })
 export class AddCvComponent {
+
+  constructor(private sanitizer: DomSanitizer) {}
+
   faPlusCircle = faPlusCircle;
   faGears = faGears;
   faStar = faStar;
   faStarHalfStroke = faStarHalfStroke;
+
+  isDragging = false;
+
+onDragOver(event: DragEvent) {
+  event.preventDefault();
+  this.isDragging = true;
+}
+
+onDragLeave(event: DragEvent) {
+  event.preventDefault();
+  this.isDragging = false;
+}
+
+onDrop(event: DragEvent) {
+  event.preventDefault();
+  this.isDragging = false;
+  const files = event.dataTransfer?.files;
+  if (files && files.length > 0) {
+    this.handleFiles(files);
+  }
+}
+
+onFileSelected(event: Event) {
+  const input = event.target as HTMLInputElement;
+  const files = input.files;
+  if (files && files.length > 0) {
+    this.handleFiles(files);
+  }
+}
+
+ pdfPreviews: { url: SafeResourceUrl, name: string }[] = [];
+
+  handleFiles(files: FileList) {
+    this.pdfPreviews = []; 
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      if (file.type !== 'application/pdf') {
+        console.warn('Odrzucono plik (nie PDF):', file.name);
+        continue;
+      }
+
+      const blobUrl = URL.createObjectURL(file);
+      const safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(blobUrl);
+      this.pdfPreviews.push({ url: safeUrl, name: file.name });
+    }
+  }
+
+  sendFiles() {
+    console.log('Wysyłanie plików:', this.pdfPreviews);
+  }
+
 }
